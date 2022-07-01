f34fad6a857cf641fa5bb31d5cce8a7c9b2b6ad8
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-07-01 10:52:10 -0300

    Codifying student class

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,5 +1,50 @@
 package com.example.demo;
 
 public class Student {
-    
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private int age;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return this.first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return this.last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
